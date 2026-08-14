1d896b4453296b178b7046bc051a736c5b5bdddd
Srijan Ravisankar <srijanvr@gmail.com>
2026-08-13 22:16:21 -0400

    feat(auth): add bcrypt password hashing and verification

--- backend/routers/secrets.py
+++ backend/routers/secrets.py
@@ -5,7 +5,6 @@ from database import get_db
 from fastapi import APIRouter, Depends, status
 from models import Secret
 from schemas.secrets import SecretCreateRequest, SecretCreateResponse
-from security.encryption import encrypt_secret
 from security.passwords import hash_password
 from sqlalchemy.orm import Session
 
@@ -21,12 +20,12 @@ def create_secret_page(secret: SecretCreateRequest, db: DbSession):
     new_secret = Secret(
         secret_prompt=secret.secret_prompt,
         secret_password_hash=hash_password(secret.secret_password),
-        secret_encrypted=encrypt_secret(
-            secret.secret_message,
-            secret.font_style,
-            secret.gif_url,
-            secret.background_colour,
-        ),
+        # secret_encrypted=encrypt_secret(
+        #     secret.secret_message,
+        #     secret.font_style,
+        #     secret.gif_url,
+        #     secret.background_colour,
+        # ),
     )
 
     db.add(new_secret)
--- backend/security/passwords.py
+++ backend/security/passwords.py
@@ -1,6 +1,16 @@
-def hash_password():
-    pass
+import bcrypt
 
 
-def verify_password():
-    pass
+def hash_password(password: str) -> str:
+    password_bytes = password.encode()
+    salt = bcrypt.gensalt()
+    hashed_password_bytes = bcrypt.hashpw(password_bytes, salt)
+    hashed_password = hashed_password_bytes.decode()
+    return hashed_password
+
+
+def verify_password(user_input: str, hashed_password: str) -> bool:
+    user_input_bytes = user_input.encode()
+    hashed_password_bytes = hashed_password.encode()
+    verification_result = bcrypt.checkpw(user_input_bytes, hashed_password_bytes)
+    return verification_result
